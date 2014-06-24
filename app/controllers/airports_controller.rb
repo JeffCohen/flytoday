5e1eabaee5702a7c9986dd43981cb71840f5b788
Jeff Cohen <jeff@starterleague.com>
2014-06-24 10:28:25 -0500

    Start using resource-based routes

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,11 +1,15 @@
 class AirportsController < ApplicationController
 
   def list
-
+    @airports = Airport.all
+    # render 'list'
+    # render :text => 'Wazzzzup!', :status => 200
+    # render :status => 302, :location => 'http://www.apple.com'
+    # redirect_to 'http://www.apple.com'
   end
 
   def show
-
+    @airport = Airport.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
-  get("/index", { :controller => "airports", :action => "list" })
-  get("/details", { :controller => "airports", :action => "show" })
+  # get "/index", :controller => "airports", :action => "list"
+  # get "/index", :to => 'airports#list'
+  # get "/details", :controller => "airports", :action => "show"
+
+  get '/' => 'airports#list'
+
+  get '/airports' => 'airports#index'
+  get '/airports/:id' => 'airports#show'
+
+  # http::localhost:3000/airports/5
+
+  # Parameters: { "id" => 5 }
 
 end
